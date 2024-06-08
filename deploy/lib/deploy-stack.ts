f0c9a3d044afbe909f6105614d62be4e25673965
Yury Hancharuk <yury.goncharuk@gmail.com>
2024-06-09 00:47:47 +0200

    Task 2(deployed infrastructure and site via AWS CDK)

---
+++ deploy/lib/deploy-stack.ts
@@ -0,0 +1,62 @@
+import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { aws_s3_deployment as s3deployment } from 'aws-cdk-lib';
+import { aws_iam as iam } from 'aws-cdk-lib';
+import { aws_cloudfront as cloudfront } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DeployStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // CloudFront Origin Access Identity
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, "rs-school-task-OAI")
+
+    // S3 bucket
+    const bucketName = "rs-school-task-s3"
+    const webIndexDocument = "index.html"
+
+    const siteBucket = new s3.Bucket(this, bucketName, {
+      bucketName: bucketName,
+      websiteIndexDocument: webIndexDocument,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true
+    });
+
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [`${siteBucket.bucketArn}/*`],
+        principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+      })
+    );
+
+    // CloudFront
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, "rs-school-task-cf", {
+      originConfigs: [{
+        s3OriginSource: {
+          s3BucketSource: siteBucket,
+          originAccessIdentity: cloudfrontOAI
+        },
+        behaviors: [{ isDefaultBehavior: true }]
+      }],
+      errorConfigurations: [{
+        errorCode: 404,
+        responseCode: 200,
+        responsePagePath: `/${webIndexDocument}`
+      }]
+    });
+
+    // S3 Deployment
+    new s3deployment.BucketDeployment(this, "rs-school-task-bucket-deploy", {
+      sources: [s3deployment.Source.asset("../dist")],
+      destinationBucket: siteBucket,
+      distribution: distribution,
+      distributionPaths: ["/*"]
+    });
+  }
+}
--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -25,6 +25,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <main>
         <Container sx={{ pb: 8 }} maxWidth="md">
           {children}
+          Nothing is here.
         </Container>
       </main>
       <Box
