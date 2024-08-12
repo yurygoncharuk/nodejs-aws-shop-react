adf24feac9eaef031a92df01e8ce2433ec0dd957
Yury Hancharuk <yury.goncharuk@gmail.com>
2024-08-12 02:29:36 +0200

    [task-10] Backend For Frontend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://r429rt8wmg.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://5mfmn5tusa.execute-api.eu-west-1.amazonaws.com/prod/api",
   import: "https://28zhrhv325.execute-api.eu-west-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://7y66uotcd0.execute-api.eu-west-1.amazonaws.com/prod",
   cart: "https://5mfmn5tusa.execute-api.eu-west-1.amazonaws.com/prod/api",
 };
 
